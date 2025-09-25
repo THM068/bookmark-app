34ced4bd940a62427a88ad451a4fb21ca098f449
admin <thando.mafela@gmail.com>
2025-09-25 22:40:09 +0100

    test

--- web/src/models/mod.rs
+++ web/src/models/mod.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 
-pub const INSERT_BOOKMARK: &str = "INSERT INTO bookmarks (url, title) VALUES ($1, $2)";
+pub const INSERT_BOOKMARK: &str = "INSERT INTO bookmarks (url, title) VALUES (?, ?)";
 
 pub const SELECT_ALL_BOOKMARKS: &str = "SELECT id, url, title FROM bookmarks ORDER BY created_at DESC";
 
